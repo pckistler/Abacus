9a887843d0d371aae1513f7181b440a857cd0d44
peter@thekistlers.com <peter@thekistlers.com>
2017-06-01 00:09:06 -0600

    Added report page

---
+++ Abacus/Controllers/ReportController.cs
@@ -0,0 +1,29 @@
+﻿using Abacus.Models;
+using Abacus.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Abacus.Controllers
+{
+    [Authorize]
+    [Authorize(Roles = "Admin,User")]
+    public class ReportController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Report
+        public ActionResult Index()
+        {
+            ReportVM report = new ReportVM();
+
+            report.Carts = db.Carts.ToList();
+            report.Payouts = db.PayOuts.ToList();
+            report.Transactions = db.TransactionRecords.ToList();
+
+            return View(report);
+        }
+    }
+}
---
+++ Abacus/ViewModel/ReportVM.cs
@@ -0,0 +1,16 @@
+﻿using Abacus.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Abacus.ViewModel
+{
+    public class ReportVM
+    {
+        public IEnumerable<Cart> Carts { get; set; }
+        public IEnumerable<Payout> Payouts { get; set; }
+
+        public IList<TransactionRecord> Transactions { get; set; }
+    }
+}
